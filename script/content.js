c0f17c0b7f47fdda4f42f3a0dad8384208deb16a
Randy Reyes <rqreyes@gmail.com>
2019-08-29 03:07:34 -0700

    add night, day, and secret features

--- script/content.js
+++ script/content.js
@@ -1,3 +1,18 @@
-chrome.runtime.onMessage.addListener(function(request) {
-  alert(request);
-})
+chrome.runtime.onMessage.addListener(function(message) {
+  if (message === 'night') {
+    $('*').css('background', '#000');
+    $('p').css('color', 'grey');
+    $('span').css('color', 'grey');
+    $('input').css('color', 'grey');
+    $('a').css('color', 'gold');
+  }
+  if (message === 'day') {
+    $('*').css('background', 'white');
+    $('p').css('color', 'grey');
+    $('span').css('color', 'grey');
+    $('a').css('color', 'blue');
+  }
+  if (message === 'secret') {
+    $('body').css('background', 'url(https://avatars3.githubusercontent.com/u/6164474?s=460&v=4)');
+  }
+});
--- script/popup.js
+++ script/popup.js
@@ -1,18 +1,21 @@
-// $('#dark').on('click', function() {
-//   $('*').css('background-color', 'yellow');
-//   console.log('hello');
-// });
-
-// console.log($('#dark'));
-// $('*').css('background-color', 'red');
-
-document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', onclick, false)
-
-  function onclick() {
+$(document).ready(function() {
+  $('#night').on('click', function() {
     chrome.tabs.query({currentWindow: true, active: true},
-    function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, 'hi')
-    })
-  }
-}, false)
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'night')
+      })
+  });
+  $('#day').on('click', function() {
+    chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'day')
+      })
+  });
+  $('#secret').on('click', function(e) {
+    e.preventDefault();
+    chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'secret')
+      })
+  });
+});
