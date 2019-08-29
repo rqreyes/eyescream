47e431cb1a5a9ece84fdca5cb0e630abc810e62c
Randy Reyes <rqreyes@gmail.com>
2019-08-29 15:26:28 -0700

    add more features and updated popup design

--- script/content.js
+++ script/content.js
@@ -1,18 +1,88 @@
 chrome.runtime.onMessage.addListener(function(message) {
+  function colorGen() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+  
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
+
+  if (message === 'secret') {
+    $('body').css('background', 'url(https://avatars3.githubusercontent.com/u/6164474?s=460&v=4)');
+  }
   if (message === 'night') {
     $('*').css('background', '#000');
+    $('h1').css('color', 'grey');
+    $('h2').css('color', 'grey');
+    $('h3').css('color', 'grey');
+    $('h4').css('color', 'grey');
+    $('h5').css('color', 'grey');
+    $('h6').css('color', 'grey');
     $('p').css('color', 'grey');
     $('span').css('color', 'grey');
     $('input').css('color', 'grey');
     $('a').css('color', 'gold');
   }
   if (message === 'day') {
-    $('*').css('background', 'white');
+    $('*').css('background', '#fff');
+    $('h1').css('color', 'grey');
+    $('h2').css('color', 'grey');
+    $('h3').css('color', 'grey');
+    $('h4').css('color', 'grey');
+    $('h5').css('color', 'grey');
+    $('h6').css('color', 'grey');
     $('p').css('color', 'grey');
     $('span').css('color', 'grey');
     $('a').css('color', 'blue');
   }
-  if (message === 'secret') {
-    $('body').css('background', 'url(https://avatars3.githubusercontent.com/u/6164474?s=460&v=4)');
+  if (message === 'disappear') {
+    $('*').css('color', 'rgba(0, 0, 0, 0)');
+  }
+  if (message === 'chalkduster') {
+    $('*').css('font-family', "chalkduster");
+  }
+  if (message === 'upside-down') {
+    let rotation = 0;
+
+    $('body').css('transform', `rotate(${rotation += 180}deg)`);
+  }
+  if (message === 'rotate') {
+    let rotation = 0;
+
+    setInterval(function(){
+      $('body').css('transform', `rotate(${rotation += 10}deg)`);
+    }, 30);
+  }
+  if (message === 'random') {
+    $('*').css('background', colorGen());
+    $('div').css('background', colorGen());
+    $('h1').css('color', colorGen());
+    $('h2').css('color', colorGen());
+    $('h3').css('color', colorGen());
+    $('h4').css('color', colorGen());
+    $('h5').css('color', colorGen());
+    $('h6').css('color', colorGen());
+    $('p').css('color', colorGen());
+    $('span').css('color', colorGen());
+    $('a').css('color', colorGen());
+  }
+  if (message === 'party') {
+    let rotation = 0;
+
+    setInterval(function(){
+      $('*').css('background', colorGen());
+      $('div').css('background', colorGen());
+      $('h1').css('color', colorGen());
+      $('h2').css('color', colorGen());
+      $('h3').css('color', colorGen());
+      $('h4').css('color', colorGen());
+      $('h5').css('color', colorGen());
+      $('h6').css('color', colorGen());
+      $('p').css('color', colorGen());
+      $('span').css('color', colorGen());
+      $('a').css('color', colorGen());
+      $('*').css('font-family', "chalkduster");
+      $('body').css('transform', `rotate(${rotation += 10}deg)`);
+    }, 30);
   }
 });
--- script/popup.js
+++ script/popup.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+  $('#secret').on('click', function(e) {
+    e.preventDefault();
+    chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'secret')
+      })
+  });
   $('#night').on('click', function() {
     chrome.tabs.query({currentWindow: true, active: true},
       function(tabs) {
@@ -11,11 +18,42 @@ $(document).ready(function() {
         chrome.tabs.sendMessage(tabs[0].id, 'day')
       })
   });
-  $('#secret').on('click', function(e) {
-    e.preventDefault();
+  $('#disappear').on('click', function() {
     chrome.tabs.query({currentWindow: true, active: true},
       function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, 'secret')
+        chrome.tabs.sendMessage(tabs[0].id, 'disappear')
+      })
+  });
+  $('#chalkduster').on('click', function() {
+    chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'chalkduster')
+      })
+  });
+  $('#upside-down').on('click', function() {
+    chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'upside-down')
+      })
+  });
+  $('#rotate').on('click', function() {
+    chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'rotate')
+      })
+  });
+  $('#random').on('click', function() {
+    chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'random')
+      })
+  });
+  $('#party').on('click', function() {
+    let audio = new Audio('https://iringtone.net/rington/file?id=8454&type=sound&name=mp3'); 
+    audio.play();
+    chrome.tabs.query({currentWindow: true, active: true},
+      function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, 'party')
       })
   });
 });
